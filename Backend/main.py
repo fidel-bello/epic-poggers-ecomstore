74dee282745eabc3de6dfd8ab0973d765d45fc17
balliam <98962893+balliam@users.noreply.github.com>
2022-04-02 19:23:03 -0400

    Added sqlite db via sqlalchemy

--- Backend/main.py
+++ Backend/main.py
@@ -1,10 +1,26 @@
-from flask import Flask
-
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
+
+db = SQLAlchemy(app)
+
+class users(db.Model):
+    _id = db.Column("id", db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+
 
 @app.route("/")
 def hello_world():
     return "<h1>Hello, World!</h1>"
 
 if __name__ == "__main__":
+    db.create_all()
     app.run()
