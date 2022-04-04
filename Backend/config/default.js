c2bfe2d67928a8873f6b4922f7b7a113700b3eef
Fidel <59423@bears.smcc.edu>
2022-04-04 19:15:55 -0400

    simple connection to nosql db, will change depending on project format

--- Backend/config/default.js
+++ Backend/config/default.js
@@ -2,4 +2,5 @@ require('dotenv').config();
 
 const config = module.exports = {
   PORT: process.env.PORT || '3006',
+  URI: process.env.URI,
 }
--- Backend/src/app/servers/express.ts
+++ Backend/src/app/servers/express.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import cors from 'cors';
+import { connectDB } from '../config/database';
 // import { Core } from '../framework';
 
 // Core.App = Core.Application.ApplicationCreate({appName: 'ecom'});
 // const { App } = Core;
 
 const expressApp = express();
+const connectionDb = connectDB();
 
 expressApp.use(cors());
 expressApp.use(express.urlencoded({extended: true}));
@@ -13,6 +15,7 @@ expressApp.use(express.json());
 
 export class HttpServer {
   private app = expressApp;
+  private connection = connectionDb;
   private _router: express.Router;
   private _port: string;
 
@@ -43,7 +46,10 @@ export class HttpServer {
     this.app.use(this._router);
   }
 
+
+
   public init() {
+    this.connection;
     this.app.listen(this.port, () => {
       console.log(`Listening on port ${this.port}\n`);
     })
