45715363e2b27fe049e741b82d0b7b3a2aca5751
Fidel <59423@bears.smcc.edu>
2022-04-14 23:01:50 -0400

    get single product with mongoose id

--- Backend/src/app/Routes/productRoutes.ts
+++ Backend/src/app/Routes/productRoutes.ts
@@ -5,6 +5,7 @@ const router = express.Router();
 const Product = new Product_Controllers();
 
 router.route('/products').get(Product.getProducts);
+router.route('/products/:id').get(Product.getSingleProduct);
 router.route('/products/new').post(Product.createProducts);
 
 export default router
--- Backend/src/app/controllers/productController.ts
+++ Backend/src/app/controllers/productController.ts
@@ -3,7 +3,7 @@ import { Product } from "../models/product";
 export class Product_Controllers 
 {
     //create products
-    public createProducts = async (req: any, res: any, next: any) => {
+    public createProducts = async (req: any, res: any, next: any): Promise<void> => {
         Product.create(req.body);
         res.status(201).json({
             success: true,
@@ -12,10 +12,10 @@ export class Product_Controllers
     };
 
     //get_products 
-    public getProducts = async(req:any, res:any, next:any) => {
+    public getProducts = async(req:any, res:any, next:any): Promise<void> => {
 
         const products = await Product.find();
-        
+
         res.status(200).json({
             success: true,
             count: products.length,
@@ -23,5 +23,21 @@ export class Product_Controllers
         })
     };
 
+    //get single product with id assigned by mongoose
+    public getSingleProduct = async (req:any, res:any, next:any): Promise<void> => {
+        
+        const product = await Product.findById(req.params.id);
+
+        if(!product)
+            return res.status(404).json({
+                success: false,
+                message: 'Product not found'
+            })
+
+        res.status(200).json({
+            success: true,
+            product
+        })
+    }
 
 }
