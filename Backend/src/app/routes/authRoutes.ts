fac73c834e79522858d32ab0a32a560aa63166c6
Fidel <59423@bears.smcc.edu>
2022-05-08 03:25:01 -0400

    login user

--- Backend/src/app/routes/authRoutes.ts
+++ Backend/src/app/routes/authRoutes.ts
@@ -6,6 +6,7 @@ const router = express.Router();
 const User = new Auth_Controllers();
 
 router.route('/register').post(User.registerUser);
+router.route('/login').post(User.loginUser);
 
 export default router
 
