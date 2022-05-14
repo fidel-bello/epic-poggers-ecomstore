f54d514dcb701d2f74826f5c36273a139bc5c09a
ivan-ngchakming <ivan.ng.chak.ming@gmail.com>
2022-05-14 14:39:15 +0800

    feat: product pagination

--- Frontend/src/pages/Home.tsx
+++ Frontend/src/pages/Home.tsx
@@ -1,45 +1,84 @@
-import { useQuery } from 'react-query';
-import axios from 'axios';
 import {
-  Card, CardActionArea, CardContent, CardMedia, Container, Grid, Typography,
+  Box,
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Container,
+  Grid,
+  Pagination,
+  Typography,
 } from '@mui/material';
+import axios from 'axios';
+import { ChangeEvent, useState } from 'react';
+import { useQuery } from 'react-query';
 import { Product } from '../types';
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 export default function Home() {
-  const { data, isLoading } = useQuery('get-products', () => axios.get(`${BASE_URL}/v1/products`));
+  const [page, setPage] = useState(0);
+
+  const handlePageChange = (event: ChangeEvent<unknown>, value: number) => {
+    setPage(value);
+  };
+
+  const { data, isLoading } = useQuery<{
+    count: number;
+    productCount: number;
+    products: Product[];
+  }>(['get-products', page], () => axios.get(`${BASE_URL}/v1/products?page=${page}`).then((res) => res?.data));
 
   if (!data || isLoading) return null;
 
-  const products: Product[] = data?.data.products;
+  const { products } = data;
 
   return (
-    <Container sx={{ mt: 4 }}>
-      <Grid container spacing={2}>
-        {products.map((product) => (
-          // eslint-disable-next-line no-underscore-dangle
-          <Grid item xs={12} sm={4} md={3} key={product._id}>
-            <Card>
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  height={250}
-                  image={product.images[0].url}
-                  alt={product.name}
-                />
-                <CardContent>
-                  <Typography>{product.name}</Typography>
-                  <Typography>
-                    $
-                    {product.price}
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
-    </Container>
+    <>
+      <Container sx={{ mt: 4 }}>
+        <Grid container spacing={2}>
+          {products.map((product) => (
+            // eslint-disable-next-line no-underscore-dangle
+            <Grid item xs={12} sm={4} md={3} key={product._id}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    height={250}
+                    image={product.images[0].url}
+                    alt={product.name}
+                  />
+                  <CardContent>
+                    <Typography>{product.name}</Typography>
+                    <Typography>
+                      $
+                      {product.price}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+      <Box
+        position="fixed"
+        display="flex"
+        justifyContent="center"
+        bottom={0}
+        width="100%"
+        mb={4}
+      >
+        <Pagination
+          count={Math.ceil(data.productCount / data.count) - 1}
+          page={page}
+          onChange={handlePageChange}
+          sx={{
+            borderRadius: 1,
+            p: 1,
+          }}
+        />
+      </Box>
+    </>
   );
 }
