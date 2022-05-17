a900586e9fe0870d6b2b155b0a9077120f00e4ca
Fidel <59423@bears.smcc.edu>
2022-05-16 23:25:33 -0400

    added enum Useroles and constructor on controller

--- Backend/src/app/routes/productRoutes.ts
+++ Backend/src/app/routes/productRoutes.ts
@@ -5,14 +5,15 @@ import { Auth_Controllers } from "../controllers/authControllers";
 const router = express.Router();
 const Product = new Product_Controllers();
 const User = new Auth_Controllers();
+
 router.route('/products').get(Product.getProducts);
 router.route('/products/:id').get(Product.getSingleProduct);
 
 //admin routes
 router.route('/admin/products/:id')
-    .put(User.isAuthenticated, User.authorizeRoles('admin') , Product.updateProduct)
-    .delete(User.isAuthenticated, User.authorizeRoles('admin'), Product.deleteProduct); //could be wrong format? did this because the admin product id route is used twice
+    .put(User.isAuthenticated, User.authorizeRoles(User.UserRole.admin) , Product.updateProduct)
+    .delete(User.isAuthenticated, User.authorizeRoles(User.UserRole.admin), Product.deleteProduct); //could be wrong format? did this because the admin product id route is used twice
 
-router.route('/admin/products/new').post(User.isAuthenticated, User.authorizeRoles('admin'), Product.createProducts);
+router.route('/admin/products/new').post(User.isAuthenticated, User.authorizeRoles(), Product.createProducts);
 
 export default router
