1a2a1318567a981e92cecb07a98504ac51f61423
Fidel <59423@bears.smcc.edu>
2022-05-23 19:01:32 -0400

    handling other json web token errors;

--- Backend/src/app/middlewares/errors.ts
+++ Backend/src/app/middlewares/errors.ts
@@ -3,7 +3,7 @@ import { Error_Handler } from '../utils/errorHandling'
 
 //middleware for the Error_Handler class (express middlewares)
 
-export  = (err: Error_Handler, req: Request, res: Response, next: NextFunction): void => {
+export  = (err: Error_Handler, _req: Request, res: Response, _next: NextFunction): void => {
     err.statusCode = err.statusCode || 500;
 
     if (process.env.NODE_ENV === 'DEVELOPMENT') {
@@ -29,7 +29,24 @@ export  = (err: Error_Handler, req: Request, res: Response, next: NextFunction):
         if(err.name === 'ValidatorError') {
             const message = Object.values(err.errors instanceof Error).map(value => value.message)
             error = new Error_Handler(message, 400);
+        }
 
+        //handling jsonwebtokenerror
+        if(err.name === 'JsonWebTokenError'){
+            const message = 'token is invalid'
+            error = new Error_Handler(message, 400);
+        }
+
+        //handling duplicate mongoose id
+        if(err.code === 1100) {
+            const message = `Duplicate ${Object.keys(err.keyValue)} entered`
+            error = new Error_Handler(message, 400)
+        }
+
+        //handling token expiration
+        if(err.name === 'TokenExpiredError') {
+            const message = 'token is expired'
+            error = new Error_Handler(message, 400);
         }
 
         res.status(res.statusCode).json({
