d18bf1ec651164a53db2e5c9d8a2fc3d3becc391
Fidel <59423@bears.smcc.edu>
2022-06-11 16:35:56 -0400

    a

--- Backend/src/app/middlewares/asyncError.ts
+++ Backend/src/app/middlewares/asyncError.ts
@@ -1,5 +1,4 @@
 /* eslint-disable no-unused-vars */
-/* eslint-disable max-len */
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable import/no-unresolved */
 import { Request, Response, NextFunction } from 'express';
